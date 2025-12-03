56e04f94558632d5fd5b9d5b459be5d673022ad6
Keshanth Jude <keshanth64@gmail.com>
2025-12-03 11:28:21 +0000

    Updated host URL

--- src/actions/api.ts
+++ src/actions/api.ts
@@ -1,6 +1,6 @@
 export const API_URL = process.env.NEXT_PUBLIC_HEADLESS_CMS_API_URL ?? "https://staging.luckydaycompetitions.com";
 
-export const BASE_URL = process.env.NODE_ENV === 'development' ? 'http://localhost:3000' : 'https://lucky-days-next-js.vercel.app';
+export const BASE_URL = process.env.NODE_ENV === 'development' ? 'http://localhost:3000' : 'https://app.luckydaycompetitions.com';
 
 export const JWT_SECRET = process.env.JWT_SECRET
 
