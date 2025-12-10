1953560e1cd6d1cfc8999ac889eeea1174dfefbd
Ben Dawson <ben@drive-digital.co.uk>
2025-12-10 20:13:09 +0000

    product carousel swiper fix

--- src/components/competition/Slider.tsx
+++ src/components/competition/Slider.tsx
@@ -1,8 +1,12 @@
 'use client';
 import React from 'react';
-import Slider from 'react-slick';
+import { Navigation } from 'swiper/modules';
+import { Swiper, SwiperSlide } from 'swiper/react';
 import Image from 'next/image';
 import Fancybox from "../../components/FancyBox";
+
+import 'swiper/css';
+import 'swiper/css/navigation';
 
 interface GalleryImage {
     url: string;
@@ -28,59 +32,26 @@ const SlickGalleryWithThumbs: React.FC<Props> = ({ mainImage, galleryImages }) =
         images.push(images[0]);
     }
 
-    const settingsMain = {
-        infinite: false,
-        speed: 200,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-    };
-
-    const settingsThumbs = {
-        customPaging: function (i: number) {
-            return (
-                <a>
-                    <Image
-                        src={images[i]?.thumbnail || images[i]?.url}
-                        alt={`Thumbnail ${i + 1}`}
-                        width={100}
-                        height={100}
-                        unoptimized
-                    />
-                </a>
-            );
-        },
-        dots: false,
-        infinite:false,
-        speed: 200,
-        slidesToShow: 5,
-        slidesToScroll: 1,
-    };
-
     return (
         <div className="col-lg-6 engine-detail-left-content">
 
             {/* large image swiper */}
+            
             <div className="prize-main-slider">               
-                <Slider {...settingsMain} className="main-slider">
-                    {images.map((img, idx) => (            
-                        <div key={idx}>  
-                            <Image
-                                src={img?.url}
-                                alt={`Image ${idx + 1}`}
-                                width={745}
-                                height={497}
-                                unoptimized
-                                className='img-blur'
-                            />
-                            <Fancybox>
-                                <a href={img?.url} data-fancybox="gallery-main"
-                                    onClick={(e) => {
-                                        // allow Fancybox to handle the click (do NOT preventDefault)
-                                        // but stop propagation to parent elements if necessary
-                                        // (usually not necessary if nav buttons are outside)
-                                        e.stopPropagation();
-                                    }}
-                                >
+                <Swiper slidesPerView={1} modules={[Navigation]} navigation className="main-slider">
+                    {images.map((img, idx) => (                 
+                        <SwiperSlide key={idx}>
+                            <Fancybox>            
+                                <Image
+                                    src={img?.url}
+                                    alt={`Image ${idx + 1}`}
+                                    width={745}
+                                    height={497}
+                                    unoptimized
+                                    className='img-blur' 
+                                />
+                            
+                                <a href={img?.url} data-fancybox="gallery-main">
                                     <Image
                                         src={img?.url}
                                         alt={`Image ${idx + 1}`}
@@ -89,10 +60,10 @@ const SlickGalleryWithThumbs: React.FC<Props> = ({ mainImage, galleryImages }) =
                                         unoptimized
                                     />
                                 </a>
-                            </Fancybox>
-                        </div>
+                            </Fancybox>     
+                        </SwiperSlide>
                     ))}
-                </Slider>
+                </Swiper>
             </div>
 
             <div className="image-thumbs">
@@ -100,7 +71,7 @@ const SlickGalleryWithThumbs: React.FC<Props> = ({ mainImage, galleryImages }) =
                 {/* no swiper, grid desktop version */}
                 <div className="thumbs-desktop">
                     {images.map((img, idx) => (
-                        <Fancybox key={idx} >
+                        <Fancybox key={idx}>       
                             <a href={img?.url} data-fancybox="gallery-desktop">
                                 <Image
                                     src={img?.url}
@@ -110,27 +81,43 @@ const SlickGalleryWithThumbs: React.FC<Props> = ({ mainImage, galleryImages }) =
                                     unoptimized
                                 />
                             </a>
-                        </Fancybox>
+                        </Fancybox>  
                     ))}
                 </div>
 
                {/* swiper, mobile version */}
                <div className="thumbs-mobile">
-                    <Slider {...settingsThumbs} className="mobile-thumbs-slider">
-                        {images.map((img, idx) => (  
-                            <Fancybox key={idx}>                             
-                                <a href={img?.url} data-fancybox="gallery-mobile">
-                                    <Image
-                                        src={img?.url}
-                                        alt={`Image ${idx + 1}`}
-                                        width={745}
-                                        height={497}
-                                        unoptimized
-                                    />
-                                </a>
-                            </Fancybox>
+                    <Swiper
+                        slidesPerView={5} 
+                        spaceBetween={10}
+                        modules={[Navigation]} 
+                        navigation={{prevEl: ".swiper-button-prev-outer", nextEl: ".swiper-button-next-outer"}} 
+                        className="mobile-thumbs-slider"
+                        onInit={(swiper) => {
+                                // swiper.params.navigation.prevEl = ".swiper-button-prev-outer";
+                                // swiper.params.navigation.nextEl = ".swiper-button-next-outer";
+                                swiper.navigation.init();
+                                swiper.navigation.update();
+                            }}
+                        >
+                        {images.map((img, idx) => (                 
+                            <SwiperSlide key={idx}>
+                                <Fancybox>                                      
+                                    <a href={img?.url} data-fancybox="gallery-mobile">
+                                        <Image
+                                            src={img?.url}
+                                            alt={`Image ${idx + 1}`}
+                                            width={745}
+                                            height={497}
+                                            unoptimized
+                                        />
+                                    </a>
+                                </Fancybox>     
+                            </SwiperSlide>
                         ))}
-                    </Slider>
+                    </Swiper>
+                    <div className="swiper-button-prev swiper-button-prev-outer"></div>
+                    <div className="swiper-button-next swiper-button-next-outer"></div>
                </div>
 
             </div>
