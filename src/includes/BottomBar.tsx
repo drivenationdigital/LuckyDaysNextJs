c3aff136dd7e3b18ae18de20de23ff1610250c2d
Keshanth Jude <keshanth64@gmail.com>
2026-02-09 15:26:20 +0000

    Remove focus on back button ios

--- src/includes/BottomBar.tsx
+++ src/includes/BottomBar.tsx
@@ -7,12 +7,18 @@ import React from "react";
 export const BottomBar = () => {
     const router = useRouter();
 
+    const handleBackClick = (e: React.MouseEvent<HTMLButtonElement>) => {
+        router.back();
+        // Immediately blur the button to remove focus state on iOS
+        e.currentTarget.blur();
+    };
+
     return (
         <div className="bottom-bar d-flex justify-content-around align-items-center d-md-none">
             {/* Back Button */}
             <button
                 className="bottom-item text-center btn p-0"
-                onClick={() => router.back()}
+                onClick={handleBackClick}
             >
                 <i className="fa fa-arrow-left"></i>
                 <div className="bottom-label">Back</div>
