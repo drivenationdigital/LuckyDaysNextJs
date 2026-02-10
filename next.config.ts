7f2c0e4e9968e52060831f0dbba993563e653e2c
Keshanth Jude <keshanth64@gmail.com>
2026-02-10 10:55:17 +0000

    Updated config

--- next.config.ts
+++ next.config.ts
@@ -41,6 +41,28 @@ const nextConfig: NextConfig = {
       }
     ],
   },
+  async headers() {
+    return [
+      {
+        source: '/.well-known/apple-app-site-association',
+        headers: [
+          {
+            key: 'Content-Type',
+            value: 'application/json',
+          },
+        ],
+      },
+      {
+        source: '/.well-known/assetlinks.json',
+        headers: [
+          {
+            key: 'Content-Type',
+            value: 'application/json',
+          },
+        ],
+      },
+    ];
+  }
 };
 
 export default nextConfig;
